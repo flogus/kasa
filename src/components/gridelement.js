fa77f1daea35df12f3f152dd8e0141af80dda734
flogus <fg@motionery.com>
2022-09-29 15:45:55 +0200

    feat: add picture om the grid element

--- src/components/gridelement.js
+++ src/components/gridelement.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 export default class gridelement extends Component {
-  render(props) {
+  render() {
     return (
-      // <Link className='p-3 text-kasa' to={`/detail/${this.props.id}`}>
-      <Link to={{ pathname: `/detail/${this.props.id}`}} state={{ from: "occupation " }}> 
-        <div id={this.props.id} className='flex items-end rounded-lg bg-kasa text-white p-3' style={{height: '300px'}}>
+      <Link to={{ pathname: `/fichelogement/${this.props.id}`}} title={this.props.title} state={{ from: "occupation " }}> 
+        <div id={this.props.id} className='flex flex-col items-center rounded-lg bg-kasa text-white p-3' style={{height: '170px'}}>
+            <img style={{border:'2px solid'}} src={this.props.pictures[0]} alt={this.props.title} />
             <h2>{this.props.title}</h2>
-            <p>{this.props.description}</p>
         </div>
       </Link>
     )
