0862a7cf8ce32873082938a3cd28a989a74beb14
flogus <fg@motionery.com>
2022-09-29 10:48:49 +0200

    feat: add state

--- src/components/gridelement.js
+++ src/components/gridelement.js
@@ -4,9 +4,10 @@ export default class gridelement extends Component {
   render(props) {
     return (
       // <Link className='p-3 text-kasa' to={`/detail/${this.props.id}`}>
-      <Link to={{ pathname: `/detail/${this.props.id}`, state: [{nom: 'Ford', title:`${this.props.title}`}] }}> 
+      <Link to={{ pathname: `/detail/${this.props.id}`}} state={{ from: "occupation " }}> 
         <div id={this.props.id} className='flex items-end rounded-lg bg-kasa text-white p-3' style={{height: '300px'}}>
             <h2>{this.props.title}</h2>
+            <p>{this.props.description}</p>
         </div>
       </Link>
     )
