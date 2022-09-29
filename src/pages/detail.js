c1fb482deac8932aabda619b6c03a07a84e6506e
flogus <fg@motionery.com>
2022-09-29 10:51:08 +0200

    refactor: import fichelogement

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import Header from '../components/header'
-import Detail from '../components/detail'
+import Fichelogement from '../components/fichelogement'
 import Footer from '../components/footer'
+import { useLocation } from 'react-router-dom'
 
 class detail extends Component {
     componentDidMount() {
         document.title = "Kasa > Détail"; 
     }
+    
 
-    render() {
+
+    render(props) {
         return (
             <div className='flex flex-col min-h-screen'>
                 <div className='m-10 flex-1'>
                     <Header />
+                    <h1 className='text-kasa border-solid border-2'>nom : {this.props.nom}</h1>
+                    <h1 className='text-kasa border-solid border-2'>title : {this.props.title}</h1>
+                    <h1 className='text-kasa border-solid border-2'>description : {this.props.description}</h1>
                     <div id="home">
-                        <Detail />
+                        <Fichelogement />
                     </div>
                 </div>
                 <Footer />
