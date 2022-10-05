6529c6379921f8ccf751d73ce3a4e7b36804c1b9
flogus <fg@motionery.com>
2022-10-05 12:36:34 +0200

    feat: add render as list

--- src/components/collapseblock/collapseblock.jsx
+++ src/components/collapseblock/collapseblock.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import svgUp from '../../assets/chevron-up.svg';
 import svgDown from '../../assets/chevron-down.svg';
+import uuid from 'react-native-uuid';
 
 class Collapseblock extends Component {
     constructor(props) {
@@ -14,6 +15,10 @@ class Collapseblock extends Component {
         const currentState = this.state.active;
         this.setState({ active: !currentState });
     };
+    renderList = (list) => {
+        return list.map((element) => <li key={uuid.v4()}>{element}</li>);
+    }
+
     render() {
         return (
             <div className='mb-4 bg-gray-200 rounded w-full' onClick={this.toggleClass}>
@@ -21,7 +26,9 @@ class Collapseblock extends Component {
                     <span className='text-white self-center'>{this.props.title}</span>
                     <img src={this.state.active ? svgUp: svgDown} alt={this.state.active ? "Fermer": "Ouvrir"} className='h-6 visible text-white' />
                 </div>
-                <div className={this.state.active ? 'h-0 invisible': 'p-2 visible text-kasa'}>{this.props.content}</div>
+                <div className={this.state.active ? 'h-0 invisible': 'p-2 visible text-kasa'}>
+                    {this.props.islist ? this.renderList(this.props.content):this.props.content}
+                </div>
             </div>
         );
     }
