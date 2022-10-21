c4a1f42c93dd91b69bdf2eb88de726e8d2624388
flogus <fg@motionery.com>
2022-10-21 09:22:34 +0200

    feat: add image full width

--- src/components/logement/logementVignette.jsx
+++ src/components/logement/logementVignette.jsx
@@ -8,8 +8,8 @@ function LogementVignette(props) {
 
     return (
       <Link to={{ pathname: `/fichelogement/${id}`}} title={title} state={{ from: "occupation " }}> 
-        <div id={id} className='flex flex-col items-center rounded-lg bg-kasa text-white p-3' style={{height: '170px'}}>
-            <img style={{border:'2px solid'}} src={pictures[0]} alt={title} />
+        <div id={id} className='flex flex-col items-center rounded-lg bg-kasa text-white p-3 h-full'>
+            <img style={{border:'2px solid',maxWidth:'100%',height:'auto'}} src={pictures[0]} alt={title} />
             <h2>{title}</h2>
         </div>
       </Link>
