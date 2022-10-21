cd99d4d09689b1ec7fc8f1452566767e83891b5b
flogus <fg@motionery.com>
2022-10-21 16:02:43 +0200

    style: just styling modification

--- src/kasarouter.jsx
+++ src/kasarouter.jsx
@@ -6,17 +6,15 @@ import AproposPage from './pages/apropos'
 import HomePage from './pages/home'
 import FichePage from './pages/fichelogement'
 import ErrorPage from './pages/error'
-
-
 class kasarouter extends Component {
     render() {
         return (
             <Router>
                 <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/apropos" element={<AproposPage />} />
-                <Route path="/fichelogement/:id" element={<FichePage />} />
-                <Route path="*" element={<ErrorPage />} />
+                    <Route path="*" element={<ErrorPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/apropos" element={<AproposPage />} />
+                    <Route path="/fichelogement/:id" element={<FichePage />} />
                 </Routes>
             </Router>
         );
