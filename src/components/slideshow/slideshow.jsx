bebcd6958f592aa793ddc6858f799ccd9f2f428c
flogus <fg@motionery.com>
2022-10-21 10:59:48 +0200

    feat: add arrow navigation for the slideshow

--- src/components/slideshow/slideshow.jsx
+++ src/components/slideshow/slideshow.jsx
@@ -8,6 +8,15 @@ function Slideshow(props) {
     const [currentindex, setCurrentIndex] = useState(0)
     const [currentimagesrc, setCurrentimagesrc] = useState(imagesData[0])
     
+    const handleKeyDown = evt => {
+        if (evt.key === 'ArrowLeft') {
+            imagePrev()
+        }
+        if (evt.key === 'ArrowRight') {
+            imageNext()
+        }
+      };
+
     const imagePrev = () => {
         if(currentindex === 0){
             setCurrentIndex(maximage - 1)
@@ -29,12 +38,12 @@ function Slideshow(props) {
 
     return (
         <div className='w-full relative'>
-            <a href='#precedent' className='absolute left-0 top-0 h-full flex items-center pl-10' onClick={imagePrev}>
+            <a href='#precedent' className='absolute left-0 top-0 h-full flex items-center pl-10' onClick={imagePrev} onKeyDown={handleKeyDown}>
                 <img className='h-16 opacity-50 hover:opacity-100' src={previous} alt='Précédent'/>
             </a>
             <img className='w-full h-auto h-80 rounded-lg object-cover' src={currentimagesrc} alt={currentimagesrc} />
             <div className='absolute bottom-0 flex w-full justify-center pb-5 text-white'>{currentindex+1}/{maximage}</div>
-            <a href='#suivant' className='absolute right-0 top-0 h-full flex items-center pr-10' onClick={imageNext}>
+            <a href='#suivant' className='absolute right-0 top-0 h-full flex items-center pr-10' onClick={imageNext} onKeyDown={handleKeyDown}>
                 <img className='h-16 opacity-50 hover:opacity-100' src={next} alt='Suivant'/>
             </a>
         </div>
